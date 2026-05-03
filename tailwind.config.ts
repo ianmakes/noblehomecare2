c95694e33a7c14c871404ff570992f0dc21447d2
STEVE IAN M. <online.ianmakes@gmail.com>
2026-05-03 18:23:07 +0300

    Revert "Implement boxy design aesthetic by overriding all border radii to square edges across the website"
    This reverts commit e34799001bff7b1a1e593c4b7592729844cd9856.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -65,15 +65,9 @@ export default {
 				'healthcare-gray-light': 'hsl(var(--healthcare-gray-light))',
 			},
 			borderRadius: {
-				none: '0',
-				sm: '0',
-				DEFAULT: '0',
-				md: '0',
-				lg: '0',
-				xl: '0',
-				'2xl': '0',
-				'3xl': '0',
-				full: '0',
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
 			},
 			keyframes: {
 				'accordion-down': {
